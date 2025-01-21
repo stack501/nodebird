df02e6c46f7f2baec044816aa185a8e56159e993
H2K <stack502@h2kresarch.com>
2025-01-21 17:42:59 +0900

    index.js 모델 자동화 생성 부분 모듈화

--- models/index.js
+++ models/index.js
@@ -1,36 +1,17 @@
 const Sequelize = require('sequelize');
-const fs = require('fs');
-const path = require('path');
-const { modelName } = require('../../10_mongoose/schemas/comment');
+const { loadModels } = require('../lib/sequelize-loader');
 
 const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config.json')[env];
-const db = {};
 const sequelize = new Sequelize(
-    config.database, config.username, config.password, config,
+    config.database,
+    config.username,
+    config.password,
+    config,
 );
+
+// 분리된 로더 함수를 통해 자동 스캔 및 db 객체 획득
+const db = loadModels(sequelize, __dirname);
 db.sequelize = sequelize;
-
-const basename = path.basename(__filename); //index.js
-fs.readdirSync(__dirname)
-    .filter(file => {
-      //file.index('.') 은 숨기파일 제외 (.env같은 파일이 숨김파일)
-      //index.js 제외
-      //파일의 마지막 세자리가 .js 인 경우만 (js파일인 경우만)
-      return file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js';
-    })
-    .forEach((file) => {
-      const model = require(path.join(__dirname, file));
-      console.log(file, model.name);
-      db[model.name] = model;
-      model.initiate(sequelize);
-    });
-
-Object.keys(db).forEach(modelName => {
-  console.log(db, modelName);
-  if (db[modelName].associate){
-    db[modelName].associate(db);
-  }
-});
 
 module.exports = db;
